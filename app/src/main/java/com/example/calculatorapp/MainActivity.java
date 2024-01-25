b027ee9127e0744ba25f63d4481b5fe1315499a3
SiddhantGujrathi1 <gujrathisiddhant@gmail.com>
2024-01-25 22:24:45 +0530

    Backend Done

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     MaterialButton btn0 , btn1 , btn2 , btn3 , btn4 , btn5 , btn6 , btn7, btn8 , btn9 , DEL , OFF , ON , AC , add , multiply , substract, divide,dot,iseqaulto;
+
+    double firstNum;
+    String Operation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,120 @@ public class MainActivity extends AppCompatActivity {
         btn7  = findViewById(R.id.num7);
         btn8  = findViewById(R.id.num8);
         btn9  = findViewById(R.id.num9);
+        btn0 = findViewById(R.id.num0);
+        DEL = findViewById(R.id.del);
+        OFF = findViewById(R.id.off);
+        ON = findViewById(R.id.on);
+        AC = findViewById(R.id.AC);
+        add = findViewById(R.id.add);
+        multiply = findViewById(R.id.multiply);
+        substract = findViewById(R.id.subtract);
+        divide = findViewById(R.id.divide);
+        dot = findViewById(R.id.numdot);
+        iseqaulto = findViewById(R.id.isequalto);
+
+        TextView screen = findViewById(R.id.screen);
+
+        AC.setOnClickListener(view -> {
+            firstNum = 0;
+            screen.setText("0");
+        });
+
+        OFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.setVisibility(View.GONE);
+            }
+        });
+
+        ON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.setVisibility(View.VISIBLE);
+                screen.setText("0");
+            }
+        });
+
+
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(btn0);
+        nums.add(btn1);
+        nums.add(btn2);
+        nums.add(btn3);
+        nums.add(btn4);
+        nums.add(btn5);
+        nums.add(btn6);
+        nums.add(btn7);
+        nums.add(btn8);
+        nums.add(btn9);
+
+        for(Button b : nums){
+            b.setOnClickListener(view -> {
+                if(!screen.getText().toString().equals("0")){
+                    screen.setText(screen.getText().toString() + b.getText().toString());
+                }else{
+                    screen.setText(b.getText());
+                }
+            });
+        }
+
+        ArrayList<Button> opers = new ArrayList<>();
+        opers.add(add);
+        opers.add(multiply);
+        opers.add(substract);
+        opers.add(divide);
+
+        for(Button b : opers){
+            b.setOnClickListener(view -> {
+                firstNum = Double.parseDouble(screen.getText().toString());
+                Operation = b.getText().toString();
+                screen.setText("0");
+            });
+        }
+
+        DEL.setOnClickListener(view -> {
+            String num = screen.getText().toString();
+            if(num.length()>1){
+                screen.setText(num.substring(0,num.length()-1));
+            }else if(num.length() == 1 && !num.equals("0")){
+                screen.setText("0");
+            }
+        });
+
+
+        dot.setOnClickListener(view -> {
+            if(!screen.getText().toString().contains(".")){
+                screen.setText(screen.getText().toString()+".");
+            }
+        });
+
+        iseqaulto.setOnClickListener(view -> {
+            double secondNum = Double.parseDouble(screen.getText().toString());
+            double result;
+            switch(Operation){
+                case "+" :
+                    result = firstNum + secondNum ;
+                    break;
+
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+
+                default:
+                    result = firstNum + secondNum;
+            }
+            screen.setText(String.valueOf(result));
+            firstNum = result;
+        });
+
 
 
 
